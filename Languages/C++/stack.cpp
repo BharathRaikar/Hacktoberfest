5860e593807037266ce661d6a2290cec82935b56
BharathRaikar <50224716+BharathRaikar@users.noreply.github.com>
2019-10-15 21:23:26 +0530

    Add files via upload

--- Languages/C++/stack.cpp
+++ Languages/C++/stack.cpp
@@ -1,74 +1,81 @@
-/* C++ program to implement basic stack 
-   operations */
 #include<iostream>
 using namespace std;
-  
-
-  
-#define MAX 1000 
-  
-class Stack { 
-    int top; 
-  
-public: 
-    int a[MAX]; // Maximum size of Stack 
-  
-    Stack() { top = -1; } 
-    bool push(int x); 
-    int pop(); 
-    int peek(); 
-    bool isEmpty(); 
-}; 
-  
-bool Stack::push(int x) 
-{ 
-    if (top >= (MAX - 1)) { 
-        cout << "Stack Overflow"; 
-        return false; 
-    } 
-    else { 
-        a[++top] = x; 
-        cout << x << " pushed into stack\n"; 
-        return true; 
-    } 
-} 
-  
-int Stack::pop() 
-{ 
-    if (top < 0) { 
-        cout << "Stack Underflow"; 
-        return 0; 
-    } 
-    else { 
-        int x = a[top--]; 
-        return x; 
-    } 
-} 
-int Stack::peek() 
-{ 
-    if (top < 0) { 
-        cout << "Stack is Empty"; 
-        return 0; 
-    } 
-    else { 
-        int x = a[top]; 
-        return x; 
-    } 
-} 
-  
-bool Stack::isEmpty() 
-{ 
-    return (top < 0); 
-} 
-  
-// Driver program to test above functions 
-int main() 
-{ 
-    class Stack s; 
-    s.push(10); 
-    s.push(20); 
-    s.push(30); 
-    cout << s.pop() << " Popped from stack\n"; 
-  
-    return 0; 
-} 
+int stack[100],n=100,top=-1;
+void push(int val)
+{
+	if(top>=(n-1))
+	cout<<"stack overflow"<<endl;
+	else
+	{
+		top++;
+		stack[top]=val;
+	}
+}
+void pop()
+{
+	if(top<=-1)
+	cout<<"stack underflow"<<endl;
+	else
+	{
+		cout<<"the popped element is "<<stack[top]<<endl;
+		top--;
+	}
+}
+void display()
+{
+	if(top>=0)
+	{
+		cout<<"stack elements are:"<<endl;
+		for(int i=top;i>=0;i--)
+		cout<<stack[i]<<" ";
+		cout<<endl;
+	}
+	else
+	cout<<"stack is empty"<<endl;
+}
+int main()
+{
+	int ch,val;
+	cout<<"1) Push in stack"<<endl;
+	cout<<"2) Pop from stack"<<endl;
+	cout<<"3) Display stack"<<endl;
+	cout<<"4) Exit"<<endl;
+	do
+	{
+		cout<<"enter choice: "<<endl;
+		cin>>ch;
+		switch(ch)
+		{
+			case 1:
+			{
+				cout<<"enter value to be pushed:"<<endl;
+				cin>>val;
+				push(val);
+				break;
+			}
+			case 2:
+			{
+				pop();
+				break;
+			}
+			case 3:
+			{
+				display();
+				break;
+			}
+			case 4:
+			{
+				cout<<"Exit"<<endl;
+				break;
+			}
+			default:
+			{
+				cout<<"Invalid Choice"<<endl;
+				break;
+			}
+				
+		}
+	}
+	while(ch!=4);
+	return 0;
+}
